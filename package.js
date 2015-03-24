d2c7f0ad75de69376fa95965532ad059692f6271
rawGem <sj.browne@gatech.edu>
2015-03-24 12:34:44 -0400

    test

--- package.js
+++ package.js
@@ -30,7 +30,7 @@ Package.on_use(function(api) {
         "accounts-base",
         "softwarerero:accounts-t9n@1.0.6",
         //"iron:router@1.0.7",
-        "meteorhacks:flow-router@1.1.1",
+        "meteorhacks:flow-router@1.1.1",//test
     ], ["client", "server"]);
 
     api.imply([
